1acdb2410897aa5504c39b8f06f3a801939960e2
kppetrick <kppetrick@gmail.com>
2021-03-24 12:09:51 -0400

    footer and navbar started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    NavigationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
